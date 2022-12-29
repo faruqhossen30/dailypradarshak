03001f888ca4029168a0ac2fa0a2caab503c496d
faruqhossen30 <faruqhossen30@gmail.com>
2022-12-29 10:54:29 +0600

    site title change

--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <!-- App favicon -->
-     <link rel="shortcut icon" style="height: 50px;0 width:50px" href="{{ asset('backend/assets/images/images/Logo.png copy-01.png') }}">
+     <link rel="shortcut icon" style="height: 50px;0 width:50px" href="{{ asset('frontend/asset/img/favicon.jpg') }}">
       <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <!------font-awesome-->
@@ -21,7 +21,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 
 
-    <title>Thedailyrunnernews</title>
+    <title>দৈনিক প্রদর্শক</title>
     @yield('OG')
 
     @stack('style')
